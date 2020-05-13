38865f3dce4c9ad6a143a723ee20cc6e2e1d3cd5
J03L R0J0 <rojo@analogue.app>
2020-05-13 13:29:33 -0700

    ✨ Add LinesBg component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinesBg from '../components/custom/LinesBg/LinesBg';
 import Testimonial from '../components/sections/Testimonial';
 import Clients from '../components/sections/Clients';
 import News from '../components/sections/News';
@@ -7,7 +8,8 @@ const Home = () => {
 
   return (
     <>
-      <Testimonial className="has-bg-color-cut illustration-section-02" />
+      <LinesBg />
+      <Testimonial className="has-bg-color-cut" />
       <Clients topDivider bottomDivider />
       <News className="illustration-section-01" />
     </>
