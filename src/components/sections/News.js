959e71525068ff3166bf6838ff7fb3bcfb111eda
J03L R0J0 <rojo@analogue.app>
2020-05-19 09:48:02 -0700

    ⚡️ Update news to tales from the road

--- src/components/sections/News.js
+++ src/components/sections/News.js
@@ -65,76 +65,28 @@ class News extends React.Component {
 
               <div className="tiles-item reveal-from-bottom">
                 <div className="tiles-item-inner has-shadow">
-                  <figure className="news-item-image m-0">
-                    <Image
-                      src={require('./../../assets/images/news-image-01.jpg')}
-                      alt="News 01"
-                      width={344}
-                      height={194} />
-                  </figure>
-                  <div className="news-item-content">
-                    <div className="news-item-body">
-                      <h3 className="news-item-title h4 mt-0 mb-8">
-                        <a href="#">Blog post title</a>
-                      </h3>
-                      <p className="mb-16 text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
-                      </p>
-                    </div>
-                    <div className="news-item-more text-xs mb-8">
-                      <a href="#">Read more</a>
-                    </div>
-                  </div>
+                  <Image
+                    src="https://images.squarespace-cdn.com/content/v1/57ccae3a440243f4deda5961/1571769558085-0ED06KQ8491IN56LGLSC/ke17ZwdGBToddI8pDm48kDHPSfPanjkWqhH6pl6g5ph7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5dNb1QJw9casjKdtTg1_-y4jz4ptJBmI9gQmbjSQnNGng/HVC+Team+axe+throwing.jpg"
+                    alt="News 03"
+                  />
                 </div>
               </div>
 
               <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
-                <div className="tiles-item-inner has-shadow">
-                  <figure className="news-item-image m-0">
-                    <Image
-                      src={require('./../../assets/images/news-image-02.jpg')}
-                      alt="News 02"
-                      width={344}
-                      height={194} />
-                  </figure>
-                  <div className="news-item-content">
-                    <div className="news-item-body">
-                      <h3 className="news-item-title h4 mt-0 mb-8">
-                        <a href="#">Blog post title</a>
-                      </h3>
-                      <p className="mb-16 text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
-                      </p>
-                    </div>
-                    <div className="news-item-more text-xs mb-8">
-                      <a href="#">Read more</a>
-                    </div>
-                  </div>
+                <div className="tiles-item-inner has-shadow" style={{ height: "auto" }}>
+                  <Image
+                    src="https://images.squarespace-cdn.com/content/v1/57ccae3a440243f4deda5961/1501043997328-VL0LZBGIGHHQM8X7RLPA/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/file-9.jpeg"
+                    alt="News 03"
+                  />
                 </div>
               </div>
 
               <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
                 <div className="tiles-item-inner has-shadow">
-                  <figure className="news-item-image m-0">
-                    <Image
-                      src={require('./../../assets/images/news-image-03.jpg')}
-                      alt="News 03"
-                      width={344}
-                      height={194} />
-                  </figure>
-                  <div className="news-item-content">
-                    <div className="news-item-body">
-                      <h3 className="news-item-title h4 mt-0 mb-8">
-                        <a href="#">Blog post title</a>
-                      </h3>
-                      <p className="mb-16 text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
-                      </p>
-                    </div>
-                    <div className="news-item-more text-xs mb-8">
-                      <a href="#">Read more</a>
-                    </div>
-                  </div>
+                  <Image
+                    src="https://images.squarespace-cdn.com/content/v1/57ccae3a440243f4deda5961/1500483626674-VCECV2572CEAS37G51RQ/ke17ZwdGBToddI8pDm48kDHPSfPanjkWqhH6pl6g5ph7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5dNb1QJw9casjKdtTg1_-y4jz4ptJBmI9gQmbjSQnNGng/IMG_2732.jpg"
+                    alt="News 03"
+                  />
                 </div>
               </div>
 
