0e6927ab350e54a312f7274c8b6711c670c7651c
J03L R0J0 <rojo@analogue.app>
2020-05-19 09:31:51 -0700

    ⚡️ Design of boxes and update text

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -51,7 +51,7 @@ class Testimonial extends React.Component {
 
     const sectionHeader = {
       title: <span>We’re on a mission to build<br />a better healthcare system</span>,
-      paragraph: <span>Let's reimagine a more <strong>affordable</strong>, <strong>sustainable,</strong> and <strong>consumer-friendly</strong> future together</span>
+      paragraph: <span>Let's create a more <strong>affordable</strong>, <strong>sustainable,</strong> and <strong>consumer-friendly</strong> future together</span>
     };
 
     return (
@@ -65,15 +65,15 @@ class Testimonial extends React.Component {
             <SectionHeader data={sectionHeader} tag="h1" className="center-content invert-color reveal-from-top" data-reveal-delay="600" />
             <div className={tilesClasses}>
 
-              <div className="tiles-item reveal-from-bottom" data-reveal-container=".tiles-wrap" data-reveal-delay="1000">
+              <div className="tiles-item reveal-from-bottom" data-reveal-container=".tiles-wrap" data-reveal-delay="1400">
                 <div className="tiles-item-inner has-shadow">
                   <div className="testimonial-item-header mb-16">
-                    <AiOutlineUsergroupAdd />
-                    <h4>We help build teams.</h4>
+                    <AiOutlineLineChart />
+                    <h4>We’re investors <i>and</i> operators.</h4>
                   </div>
                   <div className="testimonial-item-content">
                     <p className="text-sm mb-0">
-                      Over 50 executive introductions hired by our porftolio companies.
+                      We’ve been investing and building healthcare software and services for 25+ years.
                     </p>
                   </div>
                 </div>
@@ -83,25 +83,11 @@ class Testimonial extends React.Component {
                 <div className="tiles-item-inner has-shadow">
                   <div className="testimonial-item-header mb-16">
                     <AiOutlineDeploymentUnit />
-                    <h4>We have the network.</h4>
+                    <h4>We have a network.</h4>
                   </div>
                   <div className="testimonial-item-content">
                     <p className="text-sm mb-0">
                       Our unique investors and friends represent the US healthcare system.
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="tiles-item reveal-from-bottom" data-reveal-container=".tiles-wrap" data-reveal-delay="1400">
-                <div className="tiles-item-inner has-shadow">
-                  <div className="testimonial-item-header mb-16">
-                    <AiOutlineLineChart />
-                    <h4>We’re also operators.</h4>
-                  </div>
-                  <div className="testimonial-item-content">
-                    <p className="text-sm mb-0">
-                      We’ve been building healthcare software and services for 25+ years.
                     </p>
                   </div>
                 </div>
@@ -116,6 +102,20 @@ class Testimonial extends React.Component {
                   <div className="testimonial-item-content">
                     <p className="text-sm mb-0">
                       Over 100 introductions that facilitated or helped maintain revenue.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="tiles-item reveal-from-bottom" data-reveal-container=".tiles-wrap" data-reveal-delay="1000">
+                <div className="tiles-item-inner has-shadow">
+                  <div className="testimonial-item-header mb-16">
+                    <AiOutlineUsergroupAdd />
+                    <h4>We help build teams.</h4>
+                  </div>
+                  <div className="testimonial-item-content">
+                    <p className="text-sm mb-0">
+                      Over 50 executive introductions hired by our porftolio companies.
                     </p>
                   </div>
                 </div>
