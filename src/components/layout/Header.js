fba3152d4989496017377785ff704958d66bc3c0
J03L R0J0 <rojo@analogue.app>
2020-07-17 12:13:07 -0700

    🔥 Remove logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
 import LogoInvert from './partials/LogoInvert';
 
 const propTypes = {
@@ -103,7 +102,7 @@ class Header extends React.Component {
             )}>
 
             <LogoInvert />
-            
+
             {!hideNav &&
               <React.Fragment>
                 <button
