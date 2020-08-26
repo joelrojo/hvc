57eabc8e1de6f7c3bfbca4b31789c4d4ed4ae9f8
J03L R0J0 <rojo@analogue.app>
2020-08-25 21:19:16 -0700

    💡 Add descriptions to company data

--- src/data/companies.js
+++ src/data/companies.js
@@ -3,60 +3,72 @@ export const companies = [
     src: require('./../assets/images/companies/teledoc.png'),
     alt: 'Teledoc',
     caption: 'Telemedicine',
+    description: 'Healthjoy offers an employee healthcare navigation platform, largely sold through brokers, that provides digital, chat-based, and telephonic advocacy services through a unified, single-view, single-sign-on benefits wallet; free, capitated telehealth services; and concierges that facilitate services such as helping to find an in-network provider, making an appointment, and providing cost estimates; drug review services to find lower cost alternatives, drug discount coupons, and lower cost pharmacies; and medical bill review and negotiations.'
   },
   {
     src: require('./../assets/images/companies/change.png'),
     alt: 'Change Healthcare',
     caption: 'Cost Transparency',
+    description: "change: healthcare was acquired by Emdeon in 2014 and is a leading provider of healthcare cost transparency and consumer engagement solutions to tens of millions of Americans."
   },
   {
     src: require('./../assets/images/companies/livongo.png'),
     alt: 'Livongo',
     caption: 'Virtual Chronic Care Management',
+    description: "Livongo is a leading provider of diabetes digital care management for employers and plans."
   },
   {
     src: require('./../assets/images/companies/aspire.png'),
     alt: 'Aspire',
     caption: 'End of Life Care Management',
+    description: "Aspire was acquired by Anthem Inc. (NYSE: ANTM) in 2018 and is a home-based provider of palliative care services."
   },
   {
     src: require('./../assets/images/companies/aperio.jpeg'),
     alt: 'Aperio',
     caption: 'Digital Pathology',
+    description: "Aperio was acquired by Leica Biosystems in 2012.  At the time, it was the world's largest provider of digital and tele-pathology solutions to hospital systems, pathologists, and reference laboratories."
   },
   {
     src: require('./../assets/images/companies/mdlive.png'),
     alt: 'MDLive',
     caption: 'Telemedicine',
+    description: "MDLIVE is one of the nation's leading and largest telehealth platforms."
   },
   {
     src: require('./../assets/images/companies/ivx_health.jpg'),
     alt: 'IVX Health',
     caption: 'Specialty Biologics Cost Reduction',
+    description: "IVX Health is a rapidly growing provider of low-cost, consumer-centric infusion centers serving patients on expensive specialty biologics with diseases such as Rheumatoid Arthritis, Psoriasis, Crohn’s Disease, Ulcerative Colitis, Multiple Sclerosis, severe Asthma, Lupus, and others. The company’s facilities are located in Class A retail settings near where patients live and work, have a medical spa/concierge look and feel, and provide best in class, retail-like administrative services to support patients and referring physicians."
   },
   {
     src: require('./../assets/images/companies/us_renal_care.jpeg'),
-    alt: 'US Renal Care',
+    alt: 'USRenal Care',
     caption: 'Kidney Care Management',
+    description: "USRenal Care was acquired by Leonard Green in 2012 and is one of the country's largest operators of dialysis clinics for patients suffering from end stage renal disease."
   },
   {
     src: require('./../assets/images/companies/zipari.png'),
     alt: 'Zipari',
     caption: 'CRM for Health Plans',
+    description: 'Zipari provides a software platform for health plans’ member, broker, provider and employer portals as well as their call centers that is powered by a robust, underlying, modern, integrated customer relationship management (CRM) technology – bringing together traditionally siloed data into a single interface for members and customer service representatives, layering in personalized, next-best actions, and facilitating better, more integrated, and intelligent constituent service.',
   },
   {
     src: require('./../assets/images/companies/ginger.png'),
     alt: 'Ginger',
     caption: 'On-Demand Virtual Mental Health',
+    description: 'Ginger is one of the nation’s leading providers of digital, on-demand behavioral health.'
   },
   {
     src: require('./../assets/images/companies/onshift.png'),
     alt: 'OnShift',
     caption: 'Post-Acute Software',
+    description: "OnShift is a rapidly growing SaaS provider of workforce optimization software for senior care facilities that is currently serving more than 10% of the nation's skilled nursing facilities and a rapidly growing percentage of assisted living facilities."
   },
   {
     src: require('./../assets/images/companies/contessa.png'),
     alt: 'Contessa',
     caption: 'Hospital at Home',
+    description: "Contessa Health is a highly innovative, market leading provider of hospital at home services. The company couples home hospitalist telemedicine capabilities with specialized home care services to treat hundreds of medical inpatient DRGs and, when appropriate, divert these patients from the inpatient hospital setting into the home. Contessa’s programs result in significant savings to payers, savings and increased profits to their hospital partners, and high quality outcomes for patients."
   },
 ]
