4e13a8c3d0e35421889aeee9cae254f409c2908f
J03L R0J0 <rojo@analogue.app>
2020-08-25 20:18:27 -0700

    🔥 Refactor data from companies

--- src/components/sections/Clients.js
+++ src/components/sections/Clients.js
@@ -5,6 +5,7 @@ import * as Scroll from 'react-scroll';
 import SectionHeader from './partials/SectionHeader';
 import { SectionProps } from '../../utils/SectionProps';
 import Button from '../elements/Button';
+import { companies } from '../../data/companies'
 
 import './Clients.scss';
 
@@ -15,69 +16,6 @@ const propTypes = {
 const defaultProps = {
   ...SectionProps.defaults
 }
-
-const companies = [
-  {
-    src: require('./../../assets/images/companies/teledoc.png'),
-    alt: 'Teledoc',
-    caption: 'Telemedicine',
-  },
-  {
-    src: require('./../../assets/images/companies/change.png'),
-    alt: 'Change Healthcare',
-    caption: 'Cost Transparency',
-  },
-  {
-    src: require('./../../assets/images/companies/livongo.png'),
-    alt: 'Livongo',
-    caption: 'Virtual Chronic Care Management',
-  },
-  {
-    src: require('./../../assets/images/companies/aspire.png'),
-    alt: 'Aspire',
-    caption: 'End of Life Care Management',
-  },
-  {
-    src: require('./../../assets/images/companies/aperio.jpeg'),
-    alt: 'Aperio',
-    caption: 'Digital Pathology',
-  },
-  {
-    src: require('./../../assets/images/companies/mdlive.png'),
-    alt: 'MDLive',
-    caption: 'Telemedicine',
-  },
-  {
-    src: require('./../../assets/images/companies/ivx_health.jpg'),
-    alt: 'IVX Health',
-    caption: 'Specialty Biologics Cost Reduction',
-  },
-  {
-    src: require('./../../assets/images/companies/us_renal_care.jpeg'),
-    alt: 'US Renal Care',
-    caption: 'Kidney Care Management',
-  },
-  {
-    src: require('./../../assets/images/companies/zipari.png'),
-    alt: 'Zipari',
-    caption: 'CRM for Health Plans',
-  },
-  {
-    src: require('./../../assets/images/companies/ginger.png'),
-    alt: 'Ginger',
-    caption: 'On-Demand Virtual Mental Health',
-  },
-  {
-    src: require('./../../assets/images/companies/onshift.png'),
-    alt: 'OnShift',
-    caption: 'Post-Acute Software',
-  },
-  {
-    src: require('./../../assets/images/companies/contessa.png'),
-    alt: 'Contessa',
-    caption: 'Hospital at Home',
-  },
-]
 
 class Clients extends React.Component {
 
