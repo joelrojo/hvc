aaca177fba242952d41acb47334b8be5178fe501
J03L R0J0 <rojo@analogue.app>
2020-08-25 21:41:45 -0700

    ⚡️ State of clicked company

--- src/views/Companies/Company.js
+++ src/views/Companies/Company.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactTitle } from 'react-meta-tags';
 import slugify from '../../utils/slugify';
 
 const Company = ({ company, index }) => {
+
+  const [clicked, setClicked] = useState(false)
+
   return (
-    <Link to={`/companies/${slugify(company.alt)}`}>
-      <>
+    <>
+      <Link to={`/companies/${slugify(company.alt)}`} onClick={() => setClicked(true)}>
         <div className="tiles-item reveal-from-bottom companyContainer" data-reveal-container=".tiles-wrap" data-reveal-delay={index*200+200}>
           <div className="tiles-item-inner">
             <img src={company.src} alt={company.alt} />
             <h4>{company.caption}</h4>
           </div>
         </div>
-        
-      </>
-    </Link>
+      </Link>
+      {clicked && <ReactTitle title={`${company.alt} | Health Velocity Capital`} />}
+    </>
   )
 }
 
