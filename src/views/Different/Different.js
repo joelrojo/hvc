c2516cc772fb00f8ad5c6a4708c78e16e7a99a22
J03L R0J0 <rojo@analogue.app>
2020-09-28 11:53:24 -0700

    ⚡️ Init Different + mobile style

--- src/views/Different/Different.js
+++ src/views/Different/Different.js
@@ -49,7 +49,54 @@ const Different = ({
       <div className="container">
         <div className={innerClasses} style={{ paddingBottom: 0 }}>
           <SectionHeader data={sectionHeader} tag="h1" className="center-content invert-color reveal-from-top" data-reveal-delay="600" />
-          <div>
+          <div className='feature-split-wrap'>
+            <div className="feature-split">
+              <div className="center-content-mobile">
+                <h2 className="mt-0 mb-16 reveal-from-bottom" data-reveal-container=".feature-split">
+                  More Industry "Firsts" for Longer
+                </h2>
+                <p className="m-0 reveal-from-bottom" data-reveal-delay="100" data-reveal-container=".feature-split">
+                  In partnership with outstanding management teams, our Principals have helped to create <strong>more new, important healthcare sectors and market leaders</strong> than almost any other investor in the country.
+                </p>
+              </div>
+              <div className='center-content-mobile reveal-scale-up'
+                data-reveal-container=".feature-split"
+                data-reveal-delay="200">
+                Quotes
+              </div>
+            </div>
+
+            <div className="feature-split">
+              <div className='center-content-mobile reveal-scale-up'
+                data-reveal-container=".feature-split"
+                data-reveal-delay="200">
+                Graphic
+              </div>
+              <div className="center-content-mobile">
+                <h2 className="mt-0 mb-16 reveal-from-bottom" data-reveal-container=".feature-split">
+                  We Leverage Some of the Strongest Relationships in the Industry to Drive Revenue, Build Teams, & Create Symbiotic Value
+                </h2>
+                <p className="m-0 reveal-from-bottom" data-reveal-delay="100" data-reveal-container=".feature-split">
+                  We work closely with a broad group of strategic investors (in addition to an outstanding group of institutional investors) and enjoy longstanding <strong>relationships and friendships executives of with hundreds of the largest and most influential healthcare organizations in the country</strong>. These organizations help us identify the key themes in which we invest; they assist with diligence; they help our portfolio companies with product development; they help us build management teams; they drive revenue to our portfolio companies; they take part in distribution relationships; and much more.
+                </p>
+              </div>
+            </div>
+
+            <div className="feature-split">
+              <div className="center-content-mobile">
+                <h2 className="mt-0 mb-16 reveal-from-bottom" data-reveal-container=".feature-split">
+                  We Are Practical Company Builders
+                </h2>
+                <p className="m-0 reveal-from-bottom" data-reveal-delay="100" data-reveal-container=".feature-split">
+                  With decades of experience, we know that most innovative, growing companies hit a bump (or five) in the road, and the line to success is never straight.  With former operating experience, we know what it takes to overcome setbacks and to accelerate growth, and as such, are ready to roll up our sleeves to help.<br /><br />We are patient investors focused on long term value creation.
+                </p>
+              </div>
+              <div className='center-content-mobile reveal-scale-up'
+                data-reveal-container=".feature-split"
+                data-reveal-delay="200">
+                Quotes
+              </div>
+            </div>
           </div>
         </div>
       </div>
