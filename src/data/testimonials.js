a5e5d21f48e00c487196720553cfd526cad01c6b
J03L R0J0 <rojo@analogue.app>
2020-09-28 12:37:01 -0700

    💡 Testimonial data

---
+++ src/data/testimonials.js
@@ -0,0 +1,32 @@
+export const testimonials = [
+  {
+    name: 'Doug Ghertner',
+    company: 'IVX Health',
+    quote: 'We’ve worked with the team at HVC at our prior company, Change Healthcare, and now at IVX Health. There’s just no investor better networked in healthcare; they have made literally hundreds of valuable introductions over the years'
+  },
+  {
+    name: 'Russ Glass',
+    company: 'Ginger',
+    quote: 'As I was evaluating potential investors for Ginger, the Health Velocity team stood out.  They were highly regarded and it was obvious they were well-networked.  They have really leaned in since their investment.  They introduced us to potential customers, distributions partners and Board members.  They also introduced us to one of our strategic investors.'
+  },
+  {
+    name: 'Mark Woodka',
+    company: 'OnShift',
+    quote: 'The HVC investors are patient, supportive, long-term partners and friends. They are also more than just money - they introduced me to our VP Sales before they even invested and since investing, they have introduced us to a key board member and several important clients.'
+  },
+  {
+    name: 'Guillaume de Zwirek',
+    company: 'Well Health',
+    quote: 'We weren’t even raising money and they introduced us to clients and board members… we were so impressed, we invited them to invest when we didn’t need the money and at a lower valuation than we could have gotten in the market. In the past year, HVC has made over 60 introductions to potential customers, partners, board members, and key employees for Well Health.'
+  },
+  {
+    name: 'Travis Messina',
+    company: 'Contessa',
+    quote: 'We actually took capital from Health Velocity Capital at a lower valuation than what other investors had offered… and it was worth it. The HVC team has helped cement our relationships with one of largest payers and one of largest health systems in the US.'
+  },
+  {
+    name: 'Mark Nathan',
+    company: 'Zipari',
+    quote: 'The Health Velocity team helped with customer introductions before they even invested.  Since joining the Board, they’ve been invaluable not only with additional customer introductions, but also sales and marketing strategy, capital raise strategy, and Advisory Board development.'
+  }
+]
