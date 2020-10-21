6468686e4de9c364e6df1c319badec4e3ffb2ddc
J03L R0J0 <rojo@analogue.app>
2020-10-21 14:13:53 -0700

    ⚡️ Bold text

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -35,7 +35,7 @@ const videoBgs = [
 
 const headers = [
   <span>More <strong>Industry "Firsts"</strong> For Longer</span>,
-  <span>More <strong>Value-Add Investors For Longer</strong></span>,
+  <span>More <strong>Value-Add Investors</strong> For Longer</span>,
   <span>Leveraged to Drive <strong>More Customer Relationships</strong></span>,
   <span>Investing with a<br /><strong>Mission</strong></span>,
 ]
