00eab02e1d05aeb9a27313b646ce8d6810de67cc
J03L R0J0 <rojo@analogue.com>
2022-12-02 09:47:09 -0800

    🔥 Remove Julie Murchinson from advisors

--- src/data/team.js
+++ src/data/team.js
@@ -254,13 +254,6 @@ export const pastAdvisors = [
     bio: "Frank Williams is the CEO and Co-Founder of Evolent Health (NYSE: EVH). Evolent helps physicians and health systems achieve superior performance through value-based care transformation with the ultimate objective of dramatically improving the health of the populations that it serves.  The company currently partners with leading health systems in over 35\r\nmarkets, manages over 3.5M lives under value-based arrangements, and employs over 3,700 professionals. \r\n\r\nPrior to Evolent, Frank also served as Chairman and CEO of The Advisory Board Company (NASDAQ: ABCO), a global research and technology firm that serves over 5,000 organizations across the Health care and Higher Education sectors. During Frank's tenure as both its\u2019 Chairman and CEO, The Advisory Board completed an initial public offering and grew to $450M+ in annual revenues with over 2,500 employees and $2B+ in market capitalization.\r\n\r\nAlong with the Evolent Health board, he currently serves on the boards of Syapse, Business Talent Group, and Peer Health Exchange and as an Advisor to TPG\u2019s Growth and Rise Funds.  A native Californian, Frank received a BA in Political Economy from UC Berkeley and an MBA from Harvard Business School.\r\n",
   },
   {
-    src: require("./../assets/images/team/julie_2.jpg"),
-    name: "Julie Murchinson",
-    original: true,
-    title: "Founding Advisory Board Member",
-    bio: "Julie Murchinson is currently an Entrepreneur in Residence at AVIA. She was formerly the Chief Executive Officer of Health Evolution, which connects the healthcare industry’s most influential leaders to inspire forward-thinking strategies and scalable solutions that drive health for us all. Prior to Health Evolution, Julie was Managing Director at Manatt Health Solutions, the policy and strategy advisory division of Manatt, Phelps, and Phillips. Previously, Julie was Vice President of Services at CareScience, a Senior Associate with Deloitte Consulting and launched her career with Ernst & Young.  Julie received her MBA in Healthcare and Operations Management from The Wharton School at the University of Pennsylvania in Philadelphia, Pennsylvania and her BA in Business Management & Accounting from Franklin and Marshall College in Lancaster, Pennsylvania, where she currently serves on the Leadership Council. Julie also serves on the Board of Directors of the National Committee for Quality Assurance (NCQA).",
-  },
-  {
     src: require("./../assets/images/team/david.jpg"),
     name: "David Felsenthal",
     title: "Founding Advisory Board Member",
