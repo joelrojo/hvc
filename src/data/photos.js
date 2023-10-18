ffc5ee340ff7e7f1e8e5dbd0a3c7382e6b721b29
J03L R0J0 <rojo@analogue.com>
2023-10-17 22:57:03 -0700

    remove tales 48 photo

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,10 +1,12 @@
 let photos = [];
-for (let i = 1; i <= 65 ; i++) {
-  photos.push({
-    src: require(`../assets/images/tales/${i}.jpg`),
-    width: 1,
-    height: 1
-  })
+for (let i = 1; i <= 65; i++) {
+  if (i !== 48) {
+    photos.push({
+      src: require(`../assets/images/tales/${i}.jpg`),
+      width: 1,
+      height: 1,
+    });
+  }
 }
 
 export default photos;
