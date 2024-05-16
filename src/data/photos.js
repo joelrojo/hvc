c4f8469fce342a3f66b6ce4d60a602190b253469
J03L R0J0 <rojo@analogue.com>
2024-05-16 16:58:04 -0700

    ♻️ Refactor photos logic

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,12 +1,13 @@
+const requireContext = require.context('../assets/images/tales', false, /\.(jpg|jpeg|png|gif)$/);
+
 let photos = [];
-for (let i = 1; i <= 65; i++) {
-  if (i !== 48) {
-    photos.push({
-      src: require(`../assets/images/tales/${i}.jpg`),
-      width: 1,
-      height: 1,
-    });
-  }
-}
+
+requireContext.keys().forEach((key) => {
+  photos.push({
+    src: requireContext(key),
+    width: 1,
+    height: 1,
+  });
+});
 
 export default photos;
